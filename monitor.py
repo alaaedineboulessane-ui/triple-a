757942fff6c3d5a9f327f0a68b246125fbe4f7ec
Anasou06 <anas.baili@laplateforme.io>
2025-12-10 09:49:39 +0100

    css

--- monitor.py
+++ monitor.py
@@ -3,5 +3,13 @@ import psutil
 import socket
 import time
 import os
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
+@app.route("/")
+def home():
+    user_name = "Alice"
+    return render_template("template.html", user_name=user_name)
+
+app.run()
