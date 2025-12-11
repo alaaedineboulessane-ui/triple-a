f75e99149930d7ca7ff4581124ce624d1516cdf2
Anasou06 <anas.baili@laplateforme.io>
2025-12-11 09:16:07 +0100

    python

--- monitor.py
+++ monitor.py
@@ -1,4 +1,22 @@
-def test():
-    for i in range(5):
-        2+1 = 5
-    return for
+from flask import Flask, render_template
+import psutil
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    ram = psutil.virtual_memory()
+    cpu = psutil.cpu_percent(interval=1)
+    disk = psutil.disk_usage('/')
+
+    data = {
+        "ram_total": round(ram.total / (1024**3), 2),  # en Go
+        "ram_utilisee": round(ram.used / (1024**3), 2),
+        "ram_pourcent": ram.percent,
+        "cpu_pourcent": cpu,
+        "disk_total": round(disk.total / (1024**3), 2),
+        "disk_utilise": round(disk.used / (1024**3), 2),
+        "disk_pourcent": disk.percent
+    }
+
+    return render_template("index.html", data=data)
